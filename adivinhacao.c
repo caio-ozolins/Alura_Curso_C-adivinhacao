80d70aadff1f63142773544fdede1a74cfc7010a
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-09-29 17:24:08 -0300

    Create adivinhacao.c

---
+++ adivinhacao.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("--------------------------------------\n");
+    wprintf(L"Bem vindo ao nosso jogo de adivinhação\n");
+    printf("--------------------------------------\n");
+
+    int numeroSecreto = 42, chute, tentativa = 1;
+
+    while (1) {
+        wprintf(L"Tentativa %d\n", tentativa);
+        wprintf(L"Chute um número positivo: \n");
+        scanf("%d", &chute);
+        if (chute < 0){
+            wprintf(L"Por favor não digite números negativos.\n");
+            continue;
+        }
+
+        int acertou = chute == numeroSecreto;
+        int maior = chute > numeroSecreto;
+        if (acertou) {
+            wprintf(L"Você acertou!\n");
+            break;
+        }
+        else if (maior) {
+            wprintf(L"Seu chute foi maior que o número secreto.\n");
+        }
+        else {
+            wprintf(L"Seu chute foi menor que o número secreto.\n");
+        }
+        tentativa++;
+    }
+    wprintf(L"Fim de jogo!\n");
+    wprintf(L"Você acertou na %dª tentativa", tentativa);
+
+    return 0;
+}
