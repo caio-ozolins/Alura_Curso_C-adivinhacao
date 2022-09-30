0707ec9b0444da74035af4cbc291464568901a2d
Caio Ozolins Mendes <caio.ozolins@gmail.com>
2022-09-30 10:52:22 -0300

    03.12 - As funções rand e srand

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <locale.h>
 
 int main()
@@ -8,12 +10,14 @@ int main()
     printf("--------------------------------------\n");
     wprintf(L"Bem vindo ao nosso jogo de adivinhação\n");
     printf("--------------------------------------\n");
-
-    int numeroSecreto = 42, chute, tentativa = 1, pontos=1000;
+    int seed = time(0);
+    srand(seed);
+    int numeroSecreto = (rand()%100+1), chute, tentativa = 1;
+    double pontos=1000;
 
     while (1) {
         wprintf(L"Tentativa %d\n", tentativa);
-        wprintf(L"Chute um número positivo: \n");
+        wprintf(L"Chute um número positivo entre 0 e 100: \n");
         scanf("%d", &chute);
         if (chute < 0){
             wprintf(L"Por favor não digite números negativos.\n");
@@ -33,12 +37,12 @@ int main()
             wprintf(L"Seu chute foi menor que o número secreto.\n");
         }
         tentativa++;
-        int pontosPerdidos = (chute - numeroSecreto) / 2;
+        double pontosPerdidos = abs(chute - numeroSecreto) / (double)2;
         pontos -= pontosPerdidos;
     }
     wprintf(L"Fim de jogo!\n");
     wprintf(L"Você acertou na %dª tentativa\n", tentativa);
-    wprintf(L"Total de pontos: %d\n", pontos);
+    wprintf(L"Total de pontos: %.1f\n", pontos);
 
     return 0;
 }
